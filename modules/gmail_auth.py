3f177fc0a1cf9537f6fb06ced5d4e108def74ba8
archiy263 <archiyadav262003@gmail.com>
2026-03-18 18:01:47 +0530

    Update gmail_auth.py

--- modules/gmail_auth.py
+++ modules/gmail_auth.py
@@ -1,17 +1,18 @@
-from flask import redirect, request, session
 import os
+from flask import redirect, request
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 CLIENT_SECRETS_FILE = os.getenv(
     "GOOGLE_CREDENTIALS_PATH",
     "credentials.json"
 )
-
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
-
-def get_flow(state=None):
+def get_flow():
+    # auto switch between local and production
     redirect_uri = os.getenv(
         "REDIRECT_URI",
         "http://localhost:5000/oauth2callback"
@@ -20,32 +21,23 @@ def get_flow(state=None):
     return Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE,
         scopes=SCOPES,
-        state=state,
         redirect_uri=redirect_uri
     )
 
-
-# STEP 1
+# redirect to Google login
 def login():
     flow = get_flow()
 
-    auth_url, state = flow.authorization_url(
+    auth_url, _ = flow.authorization_url(
         access_type='offline',
         include_granted_scopes='true'
     )
 
-    # store state in session
-    session['oauth_state'] = state
-
     return redirect(auth_url)
 
-
-# STEP 2
+# STEP 2 → Google callback → create Gmail service
 def oauth2callback():
-    state = session.get('oauth_state')
-
-    flow = get_flow(state=state)
-
+    flow = get_flow()
     flow.fetch_token(authorization_response=request.url)
 
     creds = flow.credentials
